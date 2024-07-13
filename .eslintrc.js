2ef39e1aeb16fbd330abe731ef34d1db1c9a7e0c
SarathiSubodhi <it21800450@my.sliit.lk>
2024-07-13 20:23:57 +0530

    first commit

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,4 +1,22 @@
 module.exports = {
-  root: true,
-  extends: '@react-native',
+  parser: '@babel/eslint-parser',
+  env: {
+    browser: true,
+    es6: true,
+    node: true,
+  },
+  extends: ['eslint:recommended', 'plugin:react/recommended'],
+  parserOptions: {
+    ecmaFeatures: {
+      jsx: true,
+    },
+    ecmaVersion: 2020,
+    sourceType: 'module',
+    requireConfigFile: false,
+  },
+  plugins: ['react'],
+  rules: {
+    'react/prop-types': 'off',
+  },
 };
+
